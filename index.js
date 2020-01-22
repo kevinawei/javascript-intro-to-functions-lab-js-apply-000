cba986aaaee57bffffb96ee24dd1f66fb990f238
Kaiwen Wei <kevinawei@gmail.com>
2020-01-22 06:37:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  
+}
+function logShout(string){
+  
+}
+function logWhisper(string){
+  
+}
+function sayHiToGrandma(string){
+  
+}
